0f2205368c6c61a7bce7598ad31bf6473747e199
Loosetooth <loosetooth@protonmail.com>
2023-10-28 21:32:18 +0200

    add head tags

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,7 +3,7 @@ import LatticeImage from "./components/LatticeImage"
 export default {
   docsRepositoryBase: 'https://github.com/loosetooth/epola-space-nextra/tree/main/',
   logo: <>
-    <LatticeImage width="50px"/>
+    <LatticeImage width="50px" />
     <span style={{ marginLeft: '1rem', fontWeight: 800 }}>
       The Electron-Positron Lattice Space
     </span>
@@ -13,7 +13,7 @@ export default {
   },
   footer: {
     text: (
-      <span style={{margin: "auto"}}>
+      <span style={{ margin: "auto" }}>
         Copyright © M. Simhony
       </span>
     )
@@ -26,6 +26,15 @@ export default {
   },
   search: {
     placeholder: "Search"
-  }
+  },
+  head: (
+    <>
+      <meta name="google-site-verification" content="fNUF_FP-Kmd0lXV0E8uBslSH2igwf0hrkj06EmoDOOs" />
+      <meta property="og:title" content="The Electron-Positron Lattice Space" />
+      <meta property="og:description" content="This book is designed for readers with a basic understanding of physics. It offers an alternative approach to contemporary physics, focusing on the electron-positron lattice model. This model helps explain complex physical phenomena, derive fundamental laws, and perform calculations that may seem challenging in traditional presentations. If you're looking for a more accessible way to explore these topics and understand the 'whys' behind them, this book might be a valuable resource for you." />
+      <meta property="og:image" content="https://loosetooth.github.io/epola-space-nextra/meta-image.png" />
+      <link rel="icon" type="image/png" href="/favicon.png"></link>
+    </>
+  )
   // ... other theme options
 }
