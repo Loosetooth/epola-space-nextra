6b884766a1a5ae838d77fa7c69120321340969b0
Loosetooth <loosetooth@protonmail.com>
2024-03-16 22:56:36 +0100

    remove google site verification tag

--- theme.config.jsx
+++ theme.config.jsx
@@ -29,7 +29,6 @@ export default {
   },
   head: (
     <>
-      <meta name="google-site-verification" content="fNUF_FP-Kmd0lXV0E8uBslSH2igwf0hrkj06EmoDOOs" />
       <meta property="og:title" content="The Electron-Positron Lattice Space" />
       <meta property="og:description" content="This book is designed for readers with a basic understanding of physics. It offers an alternative approach to contemporary physics, focusing on the electron-positron lattice model. This model helps explain complex physical phenomena, derive fundamental laws, and perform calculations that may seem challenging in traditional presentations. If you're looking for a more accessible way to explore these topics and understand the 'whys' behind them, this book might be a valuable resource for you." />
       <meta property="og:image" content="/epola/meta-image.png" />
