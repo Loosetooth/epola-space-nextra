7bac1961f841def85b5cfeea104c01333fcb7170
Loosetooth <loosetooth@protonmail.com>
2024-03-16 22:55:58 +0100

    change favicon and image urls

--- theme.config.jsx
+++ theme.config.jsx
@@ -32,8 +32,8 @@ export default {
       <meta name="google-site-verification" content="fNUF_FP-Kmd0lXV0E8uBslSH2igwf0hrkj06EmoDOOs" />
       <meta property="og:title" content="The Electron-Positron Lattice Space" />
       <meta property="og:description" content="This book is designed for readers with a basic understanding of physics. It offers an alternative approach to contemporary physics, focusing on the electron-positron lattice model. This model helps explain complex physical phenomena, derive fundamental laws, and perform calculations that may seem challenging in traditional presentations. If you're looking for a more accessible way to explore these topics and understand the 'whys' behind them, this book might be a valuable resource for you." />
-      <meta property="og:image" content="https://loosetooth.github.io/epola-space-nextra/meta-image.png" />
-      <link rel="icon" type="image/png" href="https://loosetooth.github.io/epola-space-nextra/favicon.png"></link>
+      <meta property="og:image" content="/epola/meta-image.png" />
+      <link rel="icon" type="image/png" href="/epola/favicon.png"></link>
       <script defer data-domain="codebenchers.com" data-api="/epola/api/event" src="/epola/js/script.js"></script>
     </>
   )
